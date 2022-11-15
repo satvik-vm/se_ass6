6eee3446da0d51d8cf501264ae41a25bf1c97fe2
Satvik Verma <75723643+satvik-vm@users.noreply.github.com>
2022-11-16 01:12:01 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -23,17 +23,17 @@ class TestSum(unittest.TestCase):
 		result = multiply(a_3, b_3)
 		self.assertEqual(result, 25)
 
-	def test_4(self):
-		a_4 = 2
-		b_4 = 5
-		result = multiply(a_4, b_4)
-		self.assertEqual(result, 20) 
+# 	def test_4(self):
+# 		a_4 = 2
+# 		b_4 = 5
+# 		result = multiply(a_4, b_4)
+# 		self.assertEqual(result, 20) 
 
-	def test_5(self):
-		a_5 = 5
-		b_5 = 4
-		result = multiply(a_5, b_5)
-		self.assertEqual(result, 25)
+# 	def test_5(self):
+# 		a_5 = 5
+# 		b_5 = 4
+# 		result = multiply(a_5, b_5)
+# 		self.assertEqual(result, 25)
 
 if __name__ == '__main__':
 	unittest.main()
