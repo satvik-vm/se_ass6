3d23590d1751fc663da681dbc0bd3b5b09b57051
satvik-vm <satvik.verma@iiitb.ac.in>
2022-11-16 00:42:49 +0530

    initial commit

---
+++ Test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# Test case for adding two numbers
+import unittest
+
+from program.py import multiply
+
+class TestSum(unittest.TestCase):
+	def test_list_int(self):
+		a_1 = 10
+		b_1 = 10
+		result = mulitply(a_1, b_1)
+		self.assertEqual(result, 100)
+		
+		a_2 = 10
+		b_2 = 5
+		result = multiply(a_2, b_2)
+		self.assertEqual(result, 50)
+
+		a_3 = 5
+		b_3 = 5
+		result = multiply(a_3, b_3)
+		self.assertEqual(result, 25)
+
+		a_4 = 2
+		b_4 = 5
+		result = multiply(a_4, b_4)
+		self.assertEqual(result, 20) 
+
+		a_5 = 5
+		b_5 = 4
+		result = multiply(a_5, b_5)
+		self.assertEqual(result, 25)
+
+if __name__ == '__main__':
+	unittest.main()
